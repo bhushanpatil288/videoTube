666513d6b5ac0016d75fe24b87cd75a0cd04a4a1
bhushanpatil288 <sharewithbhushan@gmail.com>
2026-01-19 18:53:18 +0530

    [add] - mongoose hooks and methods, jwt(jsonwebtoken)

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -14,6 +14,7 @@ updatedAt Date
 
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
     {
@@ -67,6 +68,33 @@ userSchema.pre("save", async function(next){
     
     next();
 })
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    // short lived access token
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    );
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    );
+}
 
 
 export const User = mongoose.model("User", userSchema);
